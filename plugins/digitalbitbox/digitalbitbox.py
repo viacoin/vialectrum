92e46c87d313c5a10b969ba1e009e87e0252f1d9
romanornr <romanornr@gmail.com>
2018-02-19 11:52:47 +0100

    Viacoin: fix BIP44 path for existing keys

--- plugins/digitalbitbox/digitalbitbox.py
+++ plugins/digitalbitbox/digitalbitbox.py
@@ -677,7 +677,7 @@ class DigitalBitboxPlugin(HW_PluginBase):
         client.handler = self.create_handler(wizard)
         if purpose == HWD_SETUP_NEW_WALLET:
             client.setupRunning = True
-        client.get_xpub("m/44'/2'", 'standard')
+        client.get_xpub("m/44'/14'", 'standard')
 
 
     def is_mobile_paired(self):
--- plugins/ledger/ledger.py
+++ plugins/ledger/ledger.py
@@ -529,7 +529,7 @@ class LedgerPlugin(HW_PluginBase):
         device_id = device_info.device.id_
         client = devmgr.client_by_id(device_id)
         client.handler = self.create_handler(wizard)
-        client.get_xpub("m/44'/2'", 'standard') # TODO replace by direct derivation once Nano S > 1.1
+        client.get_xpub("m/44'/14'", 'standard') # TODO replace by direct derivation once Nano S > 1.1
 
     def get_xpub(self, device_id, derivation, xtype, wizard):
         devmgr = self.device_manager()
